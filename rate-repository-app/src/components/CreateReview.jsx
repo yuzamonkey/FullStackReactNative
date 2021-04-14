b2fb01b7977fb175b63e98d9b7ec1e5ce29988b9
Jusa Mankki <jusamankki@gmail.com>
2021-04-14 11:11:20 +0300

    styling of the form

--- rate-repository-app/src/components/CreateReview.jsx
+++ rate-repository-app/src/components/CreateReview.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
-import {Text} from 'react-native'
+import { Pressable, StyleSheet, View , Text} from 'react-native'
+import { Formik } from 'formik'
+import FormikTextInput from './FormikTextInput'
+import theme from '../theme'
+
+const styles = StyleSheet.create({
+  layout: {
+    backgroundColor: 'white',
+  },
+  submitButton: {
+    backgroundColor: theme.colors.primary,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 12,
+    marginTop: 5,
+    marginBottom: 10,
+    marginRight: 10,
+    marginLeft: 10,
+    borderRadius: 5,
+  },
+  submitText: {
+    color: theme.colors.appBarText,
+    fontWeight: theme.fontWeights.bold,
+  }
+});
+
+const initialValues = {
+  ownerName: '',
+  repositoryName: '',
+  rating: '',
+  review: '',
+}
 
 const CreateReview = () => {
+  /**
+  Repository owner's username is a required string
+  Repository's name is a required string
+  Rating is a required number between 0 and 100
+  Review is a optional string
+  */
+
+  const onSubmit = async (values) => {
+    console.log("ON SUBMIT CALLED", values)
+  }
 
   return (
-    <Text>Create review component</Text>
+    <Formik initialValues={initialValues} >
+      <View style={styles.layout}>
+        <FormikTextInput name="ownerName" placeholder="Repository owner name" />
+        <FormikTextInput name="repositoryName" placeholder="Repository name" />
+        <FormikTextInput name="rating" placeholder="Rating between 0 to 100" />
+        <FormikTextInput name="review" placeholder="Review" />
+        <Pressable onPress={onSubmit}>
+          <View style={styles.submitButton}>
+            <Text style={styles.submitText}>Create a review</Text>
+          </View>
+        </Pressable>
+      </View>
+    </Formik>
   )
 }
 
