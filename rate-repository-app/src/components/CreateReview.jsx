9fb32bfefef22469b641aa5d88cf756a92335fa3
Jusa Mankki <jusamankki@gmail.com>
2021-04-14 11:15:16 +0300

    add form validation

--- rate-repository-app/src/components/CreateReview.jsx
+++ rate-repository-app/src/components/CreateReview.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Pressable, StyleSheet, View , Text} from 'react-native'
+import { Pressable, StyleSheet, View, Text } from 'react-native'
 import { Formik } from 'formik'
 import FormikTextInput from './FormikTextInput'
 import theme from '../theme'
+import * as yup from 'yup';
 
 const styles = StyleSheet.create({
   layout: {
@@ -33,6 +34,19 @@ const initialValues = {
   review: '',
 }
 
+const validationSchema = yup.object().shape({
+  ownerName: yup
+    .string()
+    .required('Repository owner name is required'),
+  repositoryName: yup
+    .string()
+    .required('Repository name is required'),
+  rating: yup
+    .string()
+    .required('Rating is required'),
+});
+
+
 const CreateReview = () => {
   /**
   Repository owner's username is a required string
@@ -46,18 +60,20 @@ const CreateReview = () => {
   }
 
   return (
-    <Formik initialValues={initialValues} >
-      <View style={styles.layout}>
-        <FormikTextInput name="ownerName" placeholder="Repository owner name" />
-        <FormikTextInput name="repositoryName" placeholder="Repository name" />
-        <FormikTextInput name="rating" placeholder="Rating between 0 to 100" />
-        <FormikTextInput name="review" placeholder="Review" />
-        <Pressable onPress={onSubmit}>
-          <View style={styles.submitButton}>
-            <Text style={styles.submitText}>Create a review</Text>
-          </View>
-        </Pressable>
-      </View>
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+      {({ handleSubmit }) =>
+        <View style={styles.layout}>
+          <FormikTextInput name="ownerName" placeholder="Repository owner name" />
+          <FormikTextInput name="repositoryName" placeholder="Repository name" />
+          <FormikTextInput name="rating" placeholder="Rating between 0 to 100" />
+          <FormikTextInput name="review" placeholder="Review" />
+          <Pressable onPress={handleSubmit}>
+            <View style={styles.submitButton}>
+              <Text style={styles.submitText}>Create a review</Text>
+            </View>
+          </Pressable>
+        </View>
+      }
     </Formik>
   )
 }
